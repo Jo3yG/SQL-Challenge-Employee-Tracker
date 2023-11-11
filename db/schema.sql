5fd3c3ada5b57050da32bde410595e5d228e7989
Jo3yG <nonuser31@yahoo.com>
2023-11-10 20:11:42 -0600

    added employee table with columns

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS employeesDB;
+CREATE DATABASE employeesDB;
+USE employeesDB;
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NULL,
+    manager_id INTEGER NULL
+);
