860607482737e77aa44c7d9695dbbe9aab4ba5a7
Jo3yG <nonuser31@yahoo.com>
2023-11-10 20:29:15 -0600

    added role tables sections

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,13 @@ VALUES
 ('Assistent Director of Nursing'),
 ('Charge Nurse'),
 ('Nurse');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Director of Nursing', 160000, 1),
+('Assistent Director of Nursing', 80000, 2),
+('Director,Charge Nurse', 70000, 3),
+('Charge Nurse', 60000, 3),
+('A-Hall Nurse', 40000, 4),
+('B-Hall Nurse', 40000, 4),
+('C-Hall Nurse', 40000, 4);
