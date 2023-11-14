2de4f30185e1997a061dd3e2cd741094b535cd7e
Jo3yG <nonuser31@yahoo.com>
2023-11-13 18:26:19 -0600

    added password

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,10 @@ require('dotenv').config();
 const connection = mysql.createConnection({
     host: 'localhost',
     port: '3306',
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
+    user: 'root',
+    password: 'Jxinoto1!',
     database: 'employeesDB'
 });
 
 module.exports = connection;
+//process.env.DB_USER
